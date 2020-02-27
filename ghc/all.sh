4c300391e7949ea96f74c3a6405d6b99229bf007
Alexander Ben Nasrallah <me@abn.sh>
2020-02-27 17:14:26 +0100

    ghc: replace versions file with variable

--- ghc/all.sh
+++ ghc/all.sh
@@ -1,3 +1,12 @@
-	while read ghc_version; \
-		do docker build -t neosimsim/alpine-ghc:$ghc_version --build-arg GHC_VERSION=$ghc_version .; \
-	done <ghc-versions
+case $# in
+	0) versions="8.8.3 8.8.2 8.8.1 8.6.5"; break ;;
+	*) versions=$*; break ;;
+esac
+
+for ghc_version in $versions
+do
+	yes | docker system prune
+	tag=neosimsim/alpine-ghc:$ghc_version
+	docker build -t $tag --build-arg GHC_VERSION=$ghc_version .
+	docker push $tag
+done
