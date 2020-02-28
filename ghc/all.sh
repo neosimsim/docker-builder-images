44cc687486c14b25e2a7fa441085b58d5b1ec4e5
Alexander Ben Nasrallah <me@abn.sh>
2020-02-28 18:16:58 +0100

    ghc: remove alpine from tag name

--- ghc/all.sh
+++ ghc/all.sh
@@ -6,7 +6,7 @@ esac
 for ghc_version in $versions
 do
 	yes | docker system prune
-	tag=neosimsim/alpine-ghc:$ghc_version
+	tag=neosimsim/ghc:$ghc_version
 	docker build -t $tag --build-arg GHC_VERSION=$ghc_version .
 	docker push $tag
 done
